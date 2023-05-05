c2bd4b84badc78db8fd4da735ed75986a9962df6
YANG-HUAIJIANG <87012936+YANG-HUAIJIANG@users.noreply.github.com>
2023-05-06 00:37:55 +0100

    Update new sobel.py

--- new sobel.py
+++ new sobel.py
@@ -1,41 +1,43 @@
 import cv2
-from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
 
-image = cv2.imread("occyte4.jpg")
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-image = cv2.GaussianBlur(gray, (5,5), 1)
-x = cv2.Sobel(image,cv2.CV_16S,1,0)
-y = cv2.Sobel(image,cv2.CV_16S,0,1)
-absX = cv2.convertScaleAbs(x) # return to uint8
-absY = cv2.convertScaleAbs(y)
-Result = cv2.addWeighted(absX,0.5,absY,0.5,0)
+#sobel test to image
+image_path = 'occyte8.jpg'
+image = cv2.imread(image_path)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+FILLTER=9
+THREHOLD=35
+AERA=22000
+blurred_image = cv2.GaussianBlur(gray_image, (FILLTER,FILLTER), 1)
+sobel_x = cv2.Sobel(blurred_image, cv2.CV_64F, 1, 0, ksize=3)
+sobel_y = cv2.Sobel(blurred_image, cv2.CV_64F, 0, 1, ksize=3)
+# Calculate the magnitude and angle of the gradients
+magnitude = np.sqrt(np.square(sobel_x) + np.square(sobel_y))
+angle = np.arctan2(sobel_y, sobel_x) * 180 / np.pi
+# Normalize the magnitude to the range [0, 255]
+result1 = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+threshold_value = THREHOLD
+_, result2 = cv2.threshold(result1, threshold_value, 255, cv2.THRESH_BINARY)
+#find contour
+def getContours(img):
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        print(area)
+        if area>AERA:
+            cv2.drawContours(imgcnt, cnt, -1, (0, 255, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            print(peri)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            print(len(approx))
 
-ret,thresh = cv2.threshold(Result, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+imgcnt=image.copy()
+getContours(result2)
 
-# 对二值化图像进行膨胀操作，去除噪点
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-dilate = cv2.dilate(thresh, kernel)
-
-# 对膨胀后的图像进行轮廓检测
-contours, hierarchy = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-# 遍历轮廓，找到卵母和极体位置
-for cnt in contours:
-    area = cv2.contourArea(cnt)
-    if area < 500:
-        continue
-    rect = cv2.minAreaRect(cnt)
-    box = cv2.boxPoints(rect)
-    box = np.int0(box)
-    cv2.drawContours(image,[box],0,(0,0,255),2)
-# cv2.imshow("absX", absX)  #x direction
-# cv2.imshow("absY", absY)   #y direction
-cv2.imshow("image",image)
-cv2.imshow('Result', Result)
-cv2.imwrite('gray oocyte4.png',image)
-cv2.imwrite('sobel oocyte4.png',Result)
+cv2.imshow('Original Image', image)
+cv2.imshow('Thresholded Image', result2)
+cv2.imshow('Image with Contours', imgcnt)
+cv2.imwrite("sobelcnt oocyte8.png",imgcnt)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
